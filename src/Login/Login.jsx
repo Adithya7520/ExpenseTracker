604e67763f8d84e0697a5411157a56e26415320a
rolin <rolinpriyadsouza@gmail.com>
2025-09-17 20:58:26 +0530

    login page css

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./Login.css"
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+        <h5>Welcome back</h5>
+        <p>Please sign in to continue</p>
+        <div className='login-input'>
+            <div>
+            <label>Email</label>
+            <input type='email' placeholder='Enter Email' className='form-control' />
+            </div>
+            <div>
+            <label>Password</label>
+            <input type='password' placeholder='Enter password' className='form-control'/>
+           </div>
+        </div>
+        <div>
+            <button className='btn-login'>Login</button>
+        </div>
+        <div className='login-createAcc'>Don't have an account?
+            {/* <Link to=""> Create Account</Link> */}
+            </div>
+    </div>
   )
 }
 
